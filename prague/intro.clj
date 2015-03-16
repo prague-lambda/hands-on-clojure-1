d6b1d119c8049ee2f5a32821356bd92214539742
Daniel Skarda <dan.skarda@gmail.com>
2015-03-16 14:13:24 +0100

    Cleanup before hands-on in Ostrava

--- prague/intro.clj
+++ prague/intro.clj
@@ -53,7 +53,7 @@ foo
 ;; (f arg1 arg2 arg3 ...)
 (+ 1 2 3)
 
-(str "Hello," " " "world!")
+(str "Hello," " " "world!")", "
 
 ;; arbitrary nesting
 (+ 1 (* 2 (count "Hello, world")))
@@ -70,6 +70,8 @@ nil
 ;; vector
 []
 [1 2 3 4]
+
+(vector 1 2 3)
 
 ;; nested vectors
 (def v1 [:foo "bar" [:baz 1]])
@@ -95,6 +97,7 @@ nil
 (first v1)
 (rest v1)
 
+
 ;; lists
 (def l1 (list 1 2 3 4))
 
@@ -112,11 +115,18 @@ nil
 ;; maps
 (def m1 {:foo 1, :bar 2, [:complex-key 1] 3})
 
+(get m1 :foo)
+(get m1 [:complex-key 1]) 3
+(get m1 :neexistuje :default)
+
 (seq m1)
 
 (get m1 :foo)
 
-(:foo m1)
+(:foo m1 )
+
+(:baz m1 :default)
+(m1 :foo)
 
 (keys m1)
 
@@ -143,8 +153,9 @@ nil
 (inc-1 2)
 
 ;; shorter
-(defn inc-1 [a]
-  (+ 1 a))
+(defn inc-1
+  "Increment a by one"
+  [a] (+ 1 a))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -156,18 +167,41 @@ nil
 
 (filter odd? [1 2 3 4 5 ])
 
-(filter (partial + 1) [1 2 3 4])
+(map (fn [a] (+ 1 a)) [1 2 3 4])
 
-(map (comp (partial + 1) (partial * 2)) [0 1 2 3 4])
+;; Functions can be also return values
 
-(map (fn [a] (+ 1 (* 2))) [0 1 2 3 4])
+(defn mypartial
+  "Make a partial application of function f"
+  [f a]
+  (fn [b] (f a b)))
 
-;; shorter
+(mypartial + 1)
+
+((mypartial + 1) 3)
+
+(defn mycomp
+  [f g]
+  (fn [x] (f (g x))))
+
+(map (mycomp (mypartial + 1)
+             (mypartial * 2))
+     [0 1 2 3 4])
+
+;; using standard functions
+(map (comp (partial + 1)
+           (partial * 2))
+     (range 5))
+
+(map (fn [a] (+ 1 (* 2 a)))
+     (range 5))
+
+;; shorter - syntactic sugar
 (map #(+ 1 (* 2 %)) [0 1 2 3 4])
 
 (into [] (map inc [1 2 3 4]))
 
-;; syntactic sugar
+;; syntactic sugar (threading)
 (->> (map inc [0 1 2 3 4])
      (into []))
 
@@ -191,21 +225,19 @@ nil
 
   :else :default-value)
 
-
 ;; loop - recur
 (loop [var 10]
   (when (pos? var)
     (prn var)
     (recur (dec var))))
 
-
 (reduce (fn [acc value]
           (if (odd? value)
             (conj acc value)
             acc))
         []
-        (range 20))
+        (range 20))[1 3 5 7 9 11 13 15 17 19]
 
 (->> (range 20)
      (filter odd?)
-     (reduce conj []))
+     (reduce conj [])) [1 3 5 7 9 11 13 15 17 19]
